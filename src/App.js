13ab0c1bfd1b0b10b8237669b65884606b2ff32c
di858mon <di858mon@gmail.com>
2021-10-14 17:55:07 +0400

    End lesson

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ export default function App() {
     alert(text);
     console.log('12');
   }
+  const h1 = React.createElement('h1', {}, 'unit_08');
+  const h2 = React.createElement('h2', { className: 'text-orange' }, 'header2');
+  const h3 = React.createElement('p', { style: { color: 'red' } }, 'header3');
+  const inpt = React.createElement('input', {"type": "text", "value":55});
+  const p1 = React.createElement('p', {}, 'Hello');
+  const p2 = React.createElement('p', {}, 'World');
+  const div = React.createElement("div", {"className" : "text-gray"}, p1, p2)
   return (
-    <div>
-      <h1>Study React</h1>
-      <div className="out"> </div>
-      <Test />
-      <p>Start editing to see some magic happen :)</p>
-      <button onClick={() => Showalert('1234')}>Click me</button>
-      <button>SuperMagic</button>
-    </div>
+    <>
+      {h1}
+      {h2}
+      {h3}
+      {inpt}
+      {div}
+    </>
   );
 }
